51d2ea7341f163e4879e5a4394d20fd5c92864f9
zeggler <zachery@pixels3dtours.com>
2019-07-18 18:36:15 -0700

    commit

---
+++ src/components/app.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom"
+
+import PortfolioContainer from "./portfolio/portfolio-container"
+import NavigationContainer from "./portfolio/navigation/navigation-container"
+import Home from "./pages/home"
+import About from "./pages/about"
+import Contact from "./pages/contact"
+import Blog from "./pages/blog"
+import PortfolioDetail from "./portfolio/portfolio-detail"
+import Auth from "./pages/auth"
+import NoMatch from "./pages/no-match";
+
+export default class App extends Component {
+
+
+  render() {
+    return (
+      <div className='container'>
+     <Router>
+        <div><h1></h1>
+        <div></div>
+        <NavigationContainer />
+
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/auth" component={Auth} />
+          <Route path="/about-me" component={About} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/blog" component={Blog} />
+          <Route
+           exact path=
+           "/portfolio/:slug" 
+           component={PortfolioDetail} 
+           />
+          <Route component={NoMatch} />
+        </Switch>
+        </div>
+      </Router>
+    </div>
+    );
+  }
+}
